9874f33255dbc94826ae36b28556e4ed5ed9e57b
Kazark <kazark@zoho.com>
2014-03-05 15:18:09 -0500

    Added skeletal Grid class

---
+++ GameOfLife.UnitTests/Events/Grid_Specs.cs
@@ -0,0 +1,17 @@
+﻿using NSpec;
+using GameOfLife.Events;
+
+namespace GameOfLife.UnitTests.Events
+{
+    class Grid_Specs : nspec
+    {
+        private Grid _subject;
+
+        public void it_is_initialized_with_number_of_rows_which_is_also_the_number_of_columns()
+        {
+            _subject = new Grid(4);
+
+            _subject.Size.should_be(4);
+        }
+    }
+}
---
+++ GameOfLife/Events/Grid.cs
@@ -0,0 +1,17 @@
+﻿namespace GameOfLife.Events
+{
+    public class Grid
+    {
+        private int _size;
+
+        public Grid(int size)
+        {
+            _size = size;
+        }
+
+        public int Size
+        {
+            get { return _size; }
+        }
+    }
+}
