a92648a19f7e5b9388636b440da5f32c7f6c962a
Kazark <kazark@zoho.com>
2014-03-06 15:11:09 -0500

    Give the PositionInGrid class an ability produce its northern neighbor

---
+++ GameOfLife.UnitTests/Events/PositionInGrid_Specs.cs
@@ -0,0 +1,25 @@
+﻿using GameOfLife.Events;
+using NSpec;
+
+namespace GameOfLife.UnitTests.Events
+{
+    class PositionInGrid_Specs : nspec
+    {
+        private PositionInGrid _subject;
+
+        void before_each()
+        {
+            _subject = new PositionInGrid
+                {
+                    row = 2,
+                    column = 0
+                };
+        }
+
+        void it_knows_the_position_of_its_northern_neighbor()
+        {
+            _subject.northernNeighbor().row.should_be(1);
+            _subject.northernNeighbor().column.should_be(0);
+        }
+    }
+}
--- GameOfLife/Events/PositionInGrid.cs
+++ GameOfLife/Events/PositionInGrid.cs
@@ -1,7 +1,39 @@
 ﻿namespace GameOfLife.Events
 {
-    class PositionInGrid
+    public class PositionInGrid
     {
+        public PositionInGrid northernNeighbor()
+        {
+            return new PositionInGrid { column = column, row = row - 1};
+        }
+        public PositionInGrid northwesternNeighbor()
+        {
+            return new PositionInGrid();
+        }
+        public PositionInGrid westernNeighbor()
+        {
+            return new PositionInGrid();
+        }
+        public PositionInGrid southwesternNeighbor()
+        {
+            return new PositionInGrid();
+        }
+        public PositionInGrid southernNeighbor()
+        {
+            return new PositionInGrid();
+        }
+        public PositionInGrid southeasternNeighbor()
+        {
+            return new PositionInGrid();
+        }
+        public PositionInGrid easternNeighbor()
+        {
+            return new PositionInGrid();
+        }
+        public PositionInGrid northeasternNeighbor()
+        {
+            return new PositionInGrid();
+        }
         public int row { get; set; }
         public int column { get; set; }
     }
