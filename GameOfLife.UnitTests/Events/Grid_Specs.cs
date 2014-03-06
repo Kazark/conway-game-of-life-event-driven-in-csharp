7847588becd2439a4ac00be05208d47f2eae67bb
Kazark <kazark@zoho.com>
2014-03-06 11:21:00 -0500

    Made Grid iterable

--- GameOfLife.UnitTests/Events/Grid_Specs.cs
+++ GameOfLife.UnitTests/Events/Grid_Specs.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using NSpec;
 using GameOfLife.Events;
 
@@ -7,15 +8,25 @@ namespace GameOfLife.UnitTests.Events
     class Grid_Specs : nspec
     {
         private Grid<int> _subject;
+        private List<int> _sourceList;
 
-        public void it_has_size_of_the_square_root_of_the_length_of_the_intialization_list()
+        void before_each()
         {
-            _subject = new Grid<int>(new List<int>
+            _sourceList = new List<int>
                 {
                     0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-                });
+                };
+            _subject = new Grid<int>(_sourceList);
+        }
 
+        void it_has_size_of_the_square_root_of_the_length_of_the_intialization_list()
+        {
             _subject.Size.should_be(3);
+        }
+
+        void it_can_be_iterated_over()
+        {
+            _subject.ToList().should_be(_sourceList);
         }
     }
 }
--- GameOfLife/Events/Grid.cs
+++ GameOfLife/Events/Grid.cs
@@ -1,20 +1,33 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace GameOfLife.Events
 {
-    public class Grid<T>
+    public class Grid<T> : IEnumerable<T>
     {
         private readonly int _size;
+        private readonly List<T> _data;
 
         public Grid(List<T> data)
         {
+            _data = data;
             _size = Convert.ToInt32(Math.Sqrt(data.Count));
         }
 
         public int Size
         {
             get { return _size; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
