f8bdf67134ac8c3417c936f2bd41259b43c28fd6
Kazark <kazark@zoho.com>
2014-03-10 16:30:22 -0400

    Added skeletal handlers for living neighbors counted messages

---
+++ GameOfLife/Core/Handlers/NextStateOfDeadCellComputer.cs
@@ -0,0 +1,13 @@
+﻿using GameOfLife.Core.Events;
+using GameOfLife.EventInfrastructure;
+
+namespace GameOfLife.Core.Handlers
+{
+    class NextStateOfDeadCellComputer : IConsume<LivingNeighborsOfDeadCellCounted>
+    {
+        public void Consume(LivingNeighborsOfDeadCellCounted eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ GameOfLife/Core/Handlers/NextStateOfLivingCellComputer.cs
@@ -0,0 +1,13 @@
+﻿using GameOfLife.Core.Events;
+using GameOfLife.EventInfrastructure;
+
+namespace GameOfLife.Core.Handlers
+{
+    class NextStateOfLivingCellComputer : IConsume<LivingNeighborsOfLiveCellCounted>
+    {
+        public void Consume(LivingNeighborsOfLiveCellCounted eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
