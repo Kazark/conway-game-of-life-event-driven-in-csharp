819805cefecaa615f67430590e67e50f3f2723a9
Kazark <kazark@zoho.com>
2014-03-10 16:53:40 -0400

    Sketched out specs of next cell state classes

--- GameOfLife.UnitTests/Core/Handlers/DetermineNextStateOfDeadCell_Specs.cs
+++ GameOfLife.UnitTests/Core/Handlers/DetermineNextStateOfDeadCell_Specs.cs
@@ -10,5 +10,29 @@ namespace GameOfLife.UnitTests.Core.Handlers
         void before_each()
         {
         }
+
+        void it_determines_that_a_live_cell_with_less_than_two_living_neighbors_dies()
+        {
+        }
+
+        void it_determines_that_a_live_cell_with_two_living_neighbors_survives()
+        {
+        }
+
+        void it_determines_that_a_live_cell_with_three_living_neighbors_survives()
+        {
+        }
+
+        void it_determines_that_a_live_cell_with_more_than_three_living_neighbors_dies()
+        {
+        }
+
+        void it_publishes_the_position_of_the_cell_if_it_dies()
+        {
+        }
+
+        void it_publishes_the_position_of_the_cell_if_it_survives()
+        {
+        }
     }
 }
--- GameOfLife.UnitTests/Core/Handlers/DetermineNextStateOfLivingCell_Specs.cs
+++ GameOfLife.UnitTests/Core/Handlers/DetermineNextStateOfLivingCell_Specs.cs
@@ -10,5 +10,25 @@ namespace GameOfLife.UnitTests.Core.Handlers
         void before_each()
         {
         }
+
+        void it_determines_that_a_dead_cell_with_less_than_three_living_neighbors_stays_dead()
+        {
+        }
+
+        void it_determines_that_a_dead_cell_with_three_living_neighbors_comes_to_life()
+        {
+        }
+
+        void it_determines_that_a_dead_cell_with_more_than_three_living_neighbors_stays_dead()
+        {
+        }
+
+        void it_publishes_position_of_the_cell_if_it_stays_dead()
+        {
+        }
+
+        void it_publishes_position_of_the_cell_if_it_comes_to_life()
+        {
+        }
     }
 }
