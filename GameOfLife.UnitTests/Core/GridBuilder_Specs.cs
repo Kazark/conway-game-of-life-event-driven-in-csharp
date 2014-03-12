5c8c44e01d9e230fa1ce0e271a285fcd94ddf4e7
Kazark <kazark@zoho.com>
2014-03-12 14:00:01 -0400

    Publish OneGenerationOfCellStatesAggregated from GridBuilder

--- GameOfLife.UnitTests/Core/GridBuilder_Specs.cs
+++ GameOfLife.UnitTests/Core/GridBuilder_Specs.cs
@@ -12,18 +12,18 @@ namespace GameOfLife.UnitTests.Core
         void before_each()
         {
             _channel = new GenericHandlerForTesting<OneGenerationOfCellStatesAggregated>();
-            _subject = new GridBuilder(_channel)
-            {
-                Size = 2
-            };
+            _subject = new GridBuilder(_channel);
+            _subject.SetGridSizeTo(2);
         }
 
-        /*void xit_should_publish_OneGenerationOfCellStatesAggregated_event_once_it_has_all_the_data_to_build_the_grid()
+        void it_should_publish_OneGenerationOfCellStatesAggregated_event_once_it_has_all_the_data_to_build_the_grid()
         {
-            _subject.markCellDeadAt(new PositionInGrid());
-            _subject.markCellAliveAt(new PositionInGrid());
-            _subject.markCellDeadAt(new PositionInGrid());
-        }*/
+            _subject.MarkCellDeadAt(new PositionInGrid());
+            _subject.MarkCellAliveAt(new PositionInGrid());
+            _subject.MarkCellAliveAt(new PositionInGrid());
+            _subject.MarkCellDeadAt(new PositionInGrid());
+            _channel.HandledEvents.Count.should_be(1);
+        }
 
         void it_should_not_publish_before_it_has_all_the_data_to_build_the_grid()
         {
--- GameOfLife/Core/GridBuilder.cs
+++ GameOfLife/Core/GridBuilder.cs
@@ -6,20 +6,38 @@ namespace GameOfLife.Core
     public class GridBuilder
     {
         private readonly IConsume<OneGenerationOfCellStatesAggregated> _channel;
-
-        public int Size { private get; set; }
+        private int _count;
+        private int _size;
 
         public GridBuilder(IConsume<OneGenerationOfCellStatesAggregated> channel)
         {
             _channel = channel;
+            _count = 0;
         }
 
-        public void MarkCellDeadAt(PositionInGrid p0)
+        public void SetGridSizeTo(int size)
         {
+            _size = size*size;
         }
 
-        public void MarkCellAliveAt(PositionInGrid positionInGrid)
+        public void MarkCellDeadAt(PositionInGrid position)
         {
+            _count++;
+            PublishAndResetIfNecessary();
+        }
+
+        public void MarkCellAliveAt(PositionInGrid position)
+        {
+            _count++;
+            PublishAndResetIfNecessary();
+        }
+
+        private void PublishAndResetIfNecessary()
+        {
+            if (_count == _size)
+            {
+                _channel.Consume(new OneGenerationOfCellStatesAggregated());
+            }
         }
     }
 }
