1eb389aff71f7a50c046a8e1ad69fe5b7eb94f60
Kazark <kazark@zoho.com>
2014-03-18 17:30:12 -0400

    Added skeletal class IsGameOscillating

---
+++ GameOfLife.UnitTests/Core/Handlers/IsGameOscillating_Specs.cs
@@ -0,0 +1,15 @@
+﻿using GameOfLife.Core.Handlers;
+using NSpec;
+
+namespace GameOfLife.UnitTests.Core.Handlers
+{
+    class IsGameOscillating_Specs : nspec
+    {
+        private IsGameOscillating _subject;
+
+        void before_each()
+        {
+            _subject = new IsGameOscillating();
+        }
+    }
+}
