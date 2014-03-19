de1052b1831cc3f1201c74512540ebd73a59f6ca
Kazark <kazark@zoho.com>
2014-03-19 17:23:26 -0400

    Established no-op cases for IsGameOscillating

--- GameOfLife.UnitTests/Core/Handlers/IsGameOscillating_Specs.cs
+++ GameOfLife.UnitTests/Core/Handlers/IsGameOscillating_Specs.cs
@@ -1,4 +1,6 @@
-﻿using GameOfLife.Core.Handlers;
+﻿using GameOfLife.Core.Events;
+using GameOfLife.Core.Handlers;
+using GameOfLife.UnitTests.EventInfrastructure;
 using NSpec;
 
 namespace GameOfLife.UnitTests.Core.Handlers
@@ -6,10 +8,26 @@ namespace GameOfLife.UnitTests.Core.Handlers
     class IsGameOscillating_Specs : nspec
     {
         private IsGameOscillating _subject;
+        private EnqueuerMock _channelMock;
 
         void before_each()
         {
-            _subject = new IsGameOscillating();
+            _channelMock = new EnqueuerMock();
+            _subject = new IsGameOscillating(_channelMock);
+        }
+
+        void it_does_nothing_if_it_has_only_received_StatisNotReached()
+        {
+            _subject.Consume(new StatisNotReached());
+
+            _channelMock.EnqueuedAnEvent().should_be_false();
+        }
+
+        void it_does_nothing_if_it_has_only_received_OneGenerationOfCellStatesAggregated()
+        {
+            _subject.Consume(new OneGenerationOfCellStatesAggregated());
+
+            _channelMock.EnqueuedAnEvent().should_be_false();
         }
     }
 }
--- GameOfLife/Core/Handlers/IsGameOscillating.cs
+++ GameOfLife/Core/Handlers/IsGameOscillating.cs
@@ -1,6 +1,23 @@
-﻿namespace GameOfLife.Core.Handlers
+﻿using GameOfLife.Core.Events;
+using GameOfLife.EventInfrastructure;
+
+namespace GameOfLife.Core.Handlers
 {
-    public class IsGameOscillating
+    public class IsGameOscillating : IConsume<StatisNotReached>, IConsume<OneGenerationOfCellStatesAggregated>
     {
+        private readonly IEnqueueEventsOnChannel _channel;
+
+        public IsGameOscillating(IEnqueueEventsOnChannel channel)
+        {
+            _channel = channel;
+        }
+
+        public void Consume(StatisNotReached eventData)
+        {
+        }
+
+        public void Consume(OneGenerationOfCellStatesAggregated eventData)
+        {
+        }
     }
 }
