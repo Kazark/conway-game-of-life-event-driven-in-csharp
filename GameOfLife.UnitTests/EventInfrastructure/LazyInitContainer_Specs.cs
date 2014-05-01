a4c06afceda95c5ca51513f37943904f6ad81be8
Kazark <kazark@zoho.com>
2014-05-01 15:21:04 -0400

    Added skeletal LazyInitContainer

---
+++ GameOfLife.UnitTests/EventInfrastructure/LazyInitContainer_Specs.cs
@@ -0,0 +1,20 @@
+﻿using GameOfLife.EventInfrastructure;
+using NSpec;
+
+namespace GameOfLife.UnitTests.EventInfrastructure
+{
+    public class LazyInitContainer_Specs : nspec
+    {
+        private LazyInitContainer _subject;
+
+        void before_each()
+        {
+            _subject = new LazyInitContainer();
+        }
+
+        void it_returns_null_when_no_injector_is_registered_for_a_type()
+        {
+            _subject.GetInstanceOf<string>().should_be(null);
+        }
+    }
+}
---
+++ GameOfLife/EventInfrastructure/LazyInitContainer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace GameOfLife.EventInfrastructure
+{
+    public class LazyInitContainer
+    {
+        public T GetInstanceOf<T>() where T : class
+        {
+            return null;
+        }
+    }
+}
